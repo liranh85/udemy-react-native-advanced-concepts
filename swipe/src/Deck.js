4a6d25ee12517d256bb85c430d1346feedf8ac4c
Liran Harary <liranh85@gmail.com>
2018-09-10 00:32:13 +0100

    Swipe app: make cards stack and swipe away, show no more cards message. Got to lecture 37

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -8,8 +8,15 @@ import {
 
 const SCREEN_WIDTH = Dimensions.get('window').width
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH
+const SWIPE_OUT_DURATION = 250
 
 class Deck extends Component {
+  static defaultProps = {
+    onSwipeLeft: () => {},
+    onSwipeRight: () => {},
+    renderNoMoreCards: () => {}
+  }
+
   constructor(props) {
     super(props)
 
@@ -21,9 +28,9 @@ class Deck extends Component {
       },
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-          console.log('swipe right')
+          this.forceSwipe('right')
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log('swipe left')
+          this.forceSwipe('left')
         } else {
           this.resetPosition()
         }
@@ -32,7 +39,23 @@ class Deck extends Component {
 
     // The documentation places the panResponder in the state, so we're following that. Although in practice, it doesn't make a lot of sense to put this instance in the state, because it doesn't change. It should just be put as `this.panResponder`.
     // Same goes for position
-    this.state = { panResponder, position }
+    this.state = { index: 0, panResponder, position }
+  }
+
+  forceSwipe (direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH
+    Animated.timing(this.state.position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete(direction))
+  }
+
+  onSwipeComplete (direction) {
+    const { data, onSwipeLeft, onSwipeRight } = this.props
+    const item = data[this.state.index]
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item)
+    this.state.position.setValue({ x: 0, y: 0 })
+    this.setState({ index: this.state.index + 1 })
   }
 
   resetPosition () {
@@ -55,28 +78,49 @@ class Deck extends Component {
   }
 
   renderCards () {
-    const { panResponder } = this.state
-    return this.props.data.map((item, index) => {
-      if (index === 0) {
+    if (this.state.index >= this.props.data.length) {
+      return this.props.renderNoMoreCards()
+    }
+
+    return this.props.data.map((item, i) => {
+      if (i < this.state.index) {
+        return null
+      }
+
+      if (i === this.state.index) {
         return (
           <Animated.View
             key={item.id}
-            style={this.getCardStyle()}
-            {...panResponder.panHandlers}
+            style={[this.getCardStyle(), styles.cardStyle]}
+            {...this.state.panResponder.panHandlers}
           >
             {this.props.renderCard(item)}
           </Animated.View>
         )
       }
 
-      return this.props.renderCard(item)
-    })
+      return (
+        <View
+          key={item.id}
+          style={styles.cardStyle}
+        >
+          {this.props.renderCard(item)}
+        </View>
+      )
+    }).reverse()
   }
 
   render() {
     return (
       <View>{this.renderCards()}</View>
     )
+  }
+}
+
+const styles = {
+  cardStyle: {
+    position: 'absolute',
+    width: SCREEN_WIDTH
   }
 }
 
